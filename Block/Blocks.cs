0c09160527a3d221391a3df6f05a7612653db0dd
Pale-Gray <gavin.adams411@gmail.com>
2024-05-30 23:42:51 -0400

    sync

--- Block/Blocks.cs
+++ Block/Blocks.cs
@@ -10,7 +10,7 @@ namespace Blockgame_OpenTK.BlockUtil
         public static Block Air = new Block("Air").SetFaceTexture(1, 0, 0).Register();   
          
         public static Block Grass = new Block("Grass").SetFaceTexture(1, 0, 0, 1, 2, 3).SetFaceTexture(0, 0, 5).SetFaceTexture(2, 0, 4).Register();
-        public static Block Dirt = new Block("Dirt").SetFaceTexture(0, 0, 0, 1, 2, 3, 4, 5).Register(); 
+        public static Block Dirt = new Block("Dirt").SetFaceTexture(0, 0, 0, 1, 2, 3, 4, 5).Register();
         public static Block Stone = new Block("Stone").SetFaceTexture(3, 0, 0, 1, 2, 3, 4, 5).Register();
             
         public readonly static Block G = new Block("") { Name = "Grass" }.Register();
